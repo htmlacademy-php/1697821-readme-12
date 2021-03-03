2701b8c8540119a05897a469c14313389096bad4
FrAeRoK32 <1fraer1@mail.ru>
2021-03-03 18:20:35 +0300

    работа с массивом

--- index.php
+++ index.php
@@ -287,6 +287,20 @@ array('Лучшие курсы','post-link','www.htmlacademy.ru','Владик',
         <span><?php echo $contain ?></span>
     </a>
 </div>
+<?php break; case 'post-video':?>
+<!--содержимое для поста-видео-->
+<div class="post-video__block">
+    <div class="post-video__preview">
+        <?=embed_youtube_cover($contain); ?>
+        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+    </div>
+    <a href="post-details.html" class="post-video__play-big button">
+        <svg class="post-video__play-big-icon" width="14" height="14">
+            <use xlink:href="#icon-video-play-big"></use>
+        </svg>
+        <span class="visually-hidden">Запустить проигрыватель</span>
+    </a>
+</div>
 <?php default: break;
 endswitch ?>
                 </div>
